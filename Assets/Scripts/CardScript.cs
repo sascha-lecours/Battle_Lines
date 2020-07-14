016d994a3b16cf95cf8038a78794e50e906aab34
sascha-lecours <sascha.lecours@gmail.com>
2020-07-13 22:57:12 -0400

    card script in progress

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class CardScript : MonoBehaviour
 {
+    public Texture cardImage;
+    public string cardName;
+    public Transform spawnSubject;
+    public int cost = 0;
+    Renderer ren;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ren = GetComponent<Renderer>();
+        ren.material.mainTexture = cardImage;
     }
 
     // Update is called once per frame
