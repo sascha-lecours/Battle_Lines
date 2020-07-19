385a08f2be535390104cd18239d6fe5ea8ed34fa
sascha-lecours <sascha.lecours@gmail.com>
2020-07-19 14:11:14 -0400

    auto-populating unit values for cards

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -1,20 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardScript : MonoBehaviour
 {
-    public Texture cardImage;
+    #region Public Properties
+    public Image cardImage;
     public string cardName;
     public Transform spawnSubject;
-    public int cost = 0;
-    Renderer ren;
+    public Text cardText;
+    #endregion
 
+    #region Private Properties
+    private UnitScript us;
+    private HealthScript hs;
+    private MoveScript ms;
+
+    private int hp;
+    private float speed;
+    private string specialText;
+    private int cost = 0;
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
-        ren = GetComponent<Renderer>();
-        ren.material.mainTexture = cardImage;
+        us = spawnSubject.GetComponent<UnitScript>();
+        if (us != null) // If the spawn subject is a unit, just use its values for the card.
+        {
+            hs = spawnSubject.GetComponent<HealthScript>();
+            ms = spawnSubject.GetComponent<MoveScript>();
+            cardImage.sprite = spawnSubject.GetComponent<SpriteRenderer>().sprite;
+            
+            // TODO: get cost from unitscript
+        }
+
+
+
+        // TODO: If it's not a unit, it's some sort of special effect. We'll need to look elsewhere then for its icon and text, etc. Probably boolean to make custom?
     }
 
     // Update is called once per frame
