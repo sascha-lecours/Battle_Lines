79ea07a0ce466a988d1af054b94bf18e6a19d31c
sascha-lecours <sascha.lecours@gmail.com>
2020-07-23 23:02:19 -0400

    Adding health bars for buildings

---
+++ Assets/Scripts/BuildingHealthBarScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingHealthBarScript : MonoBehaviour
+{
+    BuildingScript myBuildingScript;
+    Vector3 localScale;
+    private float scaleFactor = 0.5f;
+    private float healthFraction = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBuildingScript = GetComponentInParent<BuildingScript>();
+        localScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthFraction = (Mathf.Max(0, (float)myBuildingScript.captureHealth) / (float)myBuildingScript.maxCaptureHealth);
+        localScale.x = healthFraction * scaleFactor;
+        transform.localScale = localScale;
+    }
+}
